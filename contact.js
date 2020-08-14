f56de9e1d595eea51b8bd8b4434a306ec230ee93
paul <paulrooney60@gmail.com>
2020-08-14 13:41:38 +0100

    added facebook link

--- contact.js
+++ contact.js
@@ -4,7 +4,7 @@
         "phone": "07783196464",
         "mainEmail": "pr.1stinfashion@gmail.com",
         "secondaryEmail": "paulrooney60@gmail.com",
-        "facebook": "#",
+        "facebook": "https://www.facebook.com/1stinfahion-138133056227186/",
         "whatsapp": "#",
         "created_at": "2020-06-13T13:26:36.239Z",
         "updated_at": "2020-06-13T13:26:36.239Z"
